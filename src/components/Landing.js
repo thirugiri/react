fde1380f0186696d0450e9627c23373bee46bfae
thirugiri <thirugiritharan@gmail.com>
2018-02-25 20:12:17 +0530

    bg added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,13 +3,18 @@ import logo from '../logo.svg';
 import '../App.css';
 import './css/index.css';
 import NavbarMenu from './NavbarMenu';
+import BackgroundSlideshow from 'react-background-slideshow'
+
+import image1 from './img/home1.png'
+import image2 from './img/home2.jpg'
+import image3 from './img/home3.jpg'
 
 class Landing extends Component {
   render() {
     return (
-      <div>        
+      <div>
         	<NavbarMenu />
-        	
+          <BackgroundSlideshow images={[ image1, image2, image3 ]} style="z-index:-200;"/>
       </div>
     );
   }
