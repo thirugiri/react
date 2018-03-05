41589982c3af2bfe610d806745c73f44a8be2374
thirugiri <thirugiritharan@gmail.com>
2018-03-05 16:16:06 +0530

    trail2

--- index.js
+++ index.js
@@ -24,4 +24,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/blog',Blogrouter);
-app.listen(5000);
+app.listen(process.env.PORT || 5000, function() {
+	console.log('Express server is up and running!');
+  });
+  
