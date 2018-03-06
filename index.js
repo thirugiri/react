5fa5c938452dbb1d662dc75a4198f594ed2b34d6
thirugiri <thirugiritharan@gmail.com>
2018-03-06 18:10:02 +0530

    successtrail

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 mongoose.Promise=require('bluebird');
-mongoose.connect( "mongodb://csgiri95@gmail.com:giri%401995@mycluster0-shard-00-00.mongodb.net:27017,mycluster0-shard-00-01.mongodb.net:27017,mycluster0-shard-00-02.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin")
+mongoose.connect('mongodb://giri:giri@ds257838.mlab.com:57838/blogtest')
 	.then(()=> {
 		console.log('database connected sucessfully');
 	})
