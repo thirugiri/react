795cd3556d69674da30b7e033707c5ea56850334
thirugiri <thirugiritharan@gmail.com>
2018-03-13 14:06:28 +0530

    trail14

--- index.js
+++ index.js
@@ -24,5 +24,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/blog',Blogrouter);
-var port = process.env.port || 5000;
-app.listen(port);
+var port = process.env.PORT || 5000;
+app.listen(port,function(){
+	console.log("Listening to port...");
+});
